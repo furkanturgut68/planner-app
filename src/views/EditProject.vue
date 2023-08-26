1be98d16bd0db5473c7f6423b5811caf1271d016
furkanturgut68 <furkanmehmeturgut@gmail.com>
2023-08-27 01:00:27 +0300

    edit project

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -1,13 +1,88 @@
 <template>
-  <h1>Edit</h1>
+  <h1>Edit Project</h1>
+  <form @submit.prevent="handleUpdate">
+      <label>Title</label>
+      <input type="text" v-model="title" required>
+      <label>Details</label>
+      <textarea v-model="details" required></textarea>
+      <button>Update Project</button>
+    </form>
 </template>
 
 <script>
 export default {
+  props:['id'],
+  data(){
+    return {
+      title:'',
+      details:'',
+      uri:"http://localhost:3000/project/" + this.id,
+    }
+  },
+  mounted(){
+    fetch(this.uri)
+    .then(res=>res.json())
+    .then(data=>{
+      this.title = data.title;
+      this.details = data.details;
+    })
+  },
+  methods:{
+    handleUpdate(){
+      fetch(this.uri,{
+        method:'PATCH',
+        headers:{"Content-Type":"application/json"},
+        body: JSON.stringify({title:this.title,details:this.details})
+ 
+      }).then(()=> this.$router.push("/"))
+        .catch(err =>console.log("Error:"+err))
+    }
+  }
 
 }
 </script>
 
 <style>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
 
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom:3px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+textarea {
+  border:3px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+
+button {
+  display: block;
+  margin: 20px auto 0;
+  background: #76dd78;
+  color: white;
+  padding: 10px;
+  border:0;
+  border-radius: 7px;
+  font-size: 16px;
+}
 </style>
