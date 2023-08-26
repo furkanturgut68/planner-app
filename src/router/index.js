7880f1b1eae00e6c156b166c3142b7a79116e78d
furkanturgut68 <furkanmehmeturgut@gmail.com>
2023-08-26 22:17:11 +0300

    planner-app

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/add',
+    name: 'AddProject',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/AddProject.vue')
+  },
+  {
+    path: '/project/:id',
+    name: 'EditProject',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditProject.vue'),
+    props:true
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+  <h1>Home</h1>
+    <div v-if="projects.length">
+        <div v-for="project in projects" :key="project.id">
+          
+           <single-project @delete="handleDelete" :project="project"></single-project>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from '@/components/SingleProject.vue';
+export default {
+  name: 'Home',
+  components: {
+      SingleProject
+  },
+  data(){
+    return {
+      projects : []
+    }
+  },
+  methods:{
+    handleDelete(id){
+      this.projects = this.projects.filter((project)=> project.id !== id)
+    }
+
+  },
+
+  mounted(){
+    fetch("http://localhost:3000/project")
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message));
+  },
+}
+</script>
+
+
